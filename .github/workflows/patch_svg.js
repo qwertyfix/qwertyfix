66948a53b0f9a5afcafab0fb447b4e58bd3f8d80
qwertyfix <readyrade@gmail.com>
2026-02-11 14:40:56 +0500

    Update patch_svg.js

--- .github/workflows/patch_svg.js
+++ .github/workflows/patch_svg.js
@@ -3,20 +3,20 @@ import fs from "fs";
 const file = "github_stats.svg";
 let svg = fs.readFileSync(file, "utf8");
 
-// Изменяем фон и цвет текста
+// Черный фон, синий текст
 svg = svg.replace(
   /<svg/,
-  '<svg><style>.a{fill:#0d1117 !important}.f,.g,.h,.i,.k{fill:#36BCF7 !important}</style>'
+  '<svg><style>.a{fill:#000000 !important}.f,.g,.h,.i,.k{fill:#1E90FF !important}</style>'
 );
 
-// Меняем текст
+// Меняем основной текст
 svg = svg.replace("whoami", "./ctf_prep");
 svg = svg.replace("Full Stack Developer", "Binary Security Researcher");
 
 // Добавляем дополнительную информацию
 svg = svg.replace(
   "</svg>",
-  `<text x="10" y="180" fill="#36BCF7" font-size="14">CTF Team: qwertyfix</text></svg>`
+  `<text x="10" y="180" fill="#1E90FF" font-size="14">CTF Team: qwertyfix</text></svg>`
 );
 
 fs.writeFileSync(file, svg, "utf8");
